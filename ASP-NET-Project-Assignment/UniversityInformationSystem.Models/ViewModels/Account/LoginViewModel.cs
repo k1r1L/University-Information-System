703f3a53cb15bcfce1ba5e54bc20e4bd2d0867d3
Kiril Kirilov <kironakumura@abv.bg>
2017-04-10 22:01:53 +0300

    commit css

--- ASP-NET-Project-Assignment/UniversityInformationSystem.Models/ViewModels/Account/LoginViewModel.cs
+++ ASP-NET-Project-Assignment/UniversityInformationSystem.Models/ViewModels/Account/LoginViewModel.cs
@@ -11,6 +11,8 @@
 
         [Required]
         [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "The password must be between 8 and 16 symbols")]
+        [MaxLength(16, ErrorMessage = "The password must be between 8 and 16 symbols")]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
